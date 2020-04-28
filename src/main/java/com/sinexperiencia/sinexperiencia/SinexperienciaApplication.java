6ccca4512674bd4805413b9ce09dff3c27073540
Marlen <marlenarbeloalmenara@gmail.com>
2020-04-28 16:48:29 +0100

    Primera Subida

---
+++ src/main/java/com/sinexperiencia/sinexperiencia/SinexperienciaApplication.java
@@ -0,0 +1,13 @@
+package com.sinexperiencia.sinexperiencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinexperienciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinexperienciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinexperiencia/sinexperiencia/SinexperienciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinexperiencia.sinexperiencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinexperienciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
